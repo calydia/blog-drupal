47cb47ce633c79be070d5c4fc85a29389606013a
Sanna Mäkinen <sanna.makinen@gmail.com>
2021-03-07 17:27:52 +0200

    GQL module

---
+++ src/web/modules/custom/gql_api/src/Plugin/GraphQL/SchemaExtension/BlogSchemaExtension.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Drupal\gql_api\Plugin\GraphQL\SchemaExtension;
+
+use Drupal\graphql\GraphQL\ResolverBuilder;
+use Drupal\graphql\GraphQL\ResolverRegistryInterface;
+use Drupal\graphql\GraphQL\Response\ResponseInterface;
+use Drupal\graphql\Plugin\GraphQL\SchemaExtension\SdlSchemaExtensionPluginBase;
+use Drupal\gql_api\GraphQL\Response\ArticleResponse;
+use Drupal\gql_api\Wrappers\QueryConnection;
+
+/**
+ * @SchemaExtension(
+ *   id = "composable_extension",
+ *   name = "Composable Example extension",
+ *   description = "A simple extension that adds node related fields.",
+ *   schema = "composable"
+ * )
+ */
+class BlogSchemaExtension extends SdlSchemaExtensionPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function registerResolvers(ResolverRegistryInterface $registry) {
+    $builder = new ResolverBuilder();
+
+    $registry->addFieldResolver('Query', 'article',
+      $builder->produce('entity_load')
+        ->map('type', $builder->fromValue('node'))
+        ->map('bundles', $builder->fromValue(['article']))
+        ->map('id', $builder->fromArgument('id'))
+    );
+
+    $registry->addFieldResolver('Query', 'articles',
+      $builder->produce('query_articles')
+        ->map('offset', $builder->fromArgument('offset'))
+        ->map('limit', $builder->fromArgument('limit'))
+        ->map('category', $builder->fromArgument('category'))
+    );
+
+    // Create article mutation.
+    $registry->addFieldResolver('Mutation', 'createArticle',
+      $builder->produce('create_article')
+        ->map('data', $builder->fromArgument('data'))
+    );
+
+    $registry->addFieldResolver('ArticleResponse', 'article',
+      $builder->callback(function (ArticleResponse $response) {
+        return $response->article();
+      })
+    );
+
+    $registry->addFieldResolver('ArticleResponse', 'errors',
+      $builder->callback(function (ArticleResponse $response) {
+        return $response->getViolations();
+      })
+    );
+
+    $registry->addFieldResolver('Article', 'id',
+      $builder->produce('entity_id')
+        ->map('entity', $builder->fromParent())
+    );
+
+    $registry->addFieldResolver('Article', 'title',
+      $builder->compose(
+        $builder->produce('entity_label')
+          ->map('entity', $builder->fromParent())
+      )
+    );
+
+    $registry->addFieldResolver('Article', 'date',
+      $builder->produce('entity_created')
+        ->map('entity', $builder->fromParent())
+    );
+
+    $registry->addFieldResolver('Article', 'published',
+      $builder->produce('entity_published')
+        ->map('entity', $builder->fromParent())
+    );
+
+    $registry->addFieldResolver('Article', 'author',
+      $builder->compose(
+        $builder->produce('entity_owner')
+          ->map('entity', $builder->fromParent()),
+        $builder->produce('entity_label')
+          ->map('entity', $builder->fromParent())
+      )
+    );
+
+    $registry->addFieldResolver('Article', 'content',
+    $builder->produce('property_path')
+      ->map('type', $builder->fromValue('entity:node'))
+      ->map('value', $builder->fromParent())
+      ->map('path', $builder->fromValue('body.value'))
+    );
+
+    $registry->addFieldResolver('Article', 'category',
+    $builder->compose(
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:node'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_blog_category.entity')),
+      $builder->produce('entity_label')
+        ->map('entity',$builder->fromParent()))
+    );
+
+    $registry->addFieldResolver('Article', 'mainImage',
+      $builder->compose(
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:node'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_main_image.entity')),
+      $builder->produce("image_url")
+        ->map('entity',$builder->fromParent()))
+    );
+
+
+    $registry->addFieldResolver('Article', 'listingImage',
+      $builder->compose(
+      $builder->produce('property_path')
+        ->map('type', $builder->fromValue('entity:node'))
+        ->map('value', $builder->fromParent())
+        ->map('path', $builder->fromValue('field_listing_image.entity')),
+      $builder->produce("image_url")
+        ->map('entity',$builder->fromParent()))
+    );
+
+    $registry->addFieldResolver('ArticleConnection', 'total',
+      $builder->callback(function (QueryConnection $connection) {
+        return $connection->total();
+      })
+    );
+
+    $registry->addFieldResolver('ArticleConnection', 'items',
+      $builder->callback(function (QueryConnection $connection) {
+        return $connection->items();
+      })
+    );
+
+    // Response type resolver.
+    $registry->addTypeResolver('Response', [
+      __CLASS__,
+      'resolveResponse',
+    ]);
+  }
+
+  /**
+   * Resolves the response type.
+   *
+   * @param \Drupal\graphql\GraphQL\Response\ResponseInterface $response
+   *   Response object.
+   *
+   * @return string
+   *   Response type.
+   *
+   * @throws \Exception
+   *   Invalid response type.
+   */
+  public static function resolveResponse(ResponseInterface $response): string {
+    // Resolve content response.
+    if ($response instanceof ArticleResponse) {
+      return 'ArticleResponse';
+    }
+    throw new \Exception('Invalid response type.');
+  }
+
+}
